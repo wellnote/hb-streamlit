91219171b0d04ee2df9f15ec198a31c41e53b886
root <root@DESKTOP-169GR7L.localdomain>
2025-08-11 02:16:43 +0300

    init streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="HB Live Dashboard", layout="wide")
+st.title("Hello from Streamlit Cloud!")
+st.write("Подключите API и рисуйте метрики здесь.")
